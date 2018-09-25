cad31d50c253350614c73d7e06eabb964274bc21
Dominic Groshong <groshond@gmail.com>
2018-09-25 12:14:05 -0700

    Cant use php which sucks, switching to pure html

--- HW1/footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-<p>Copyright 2018 Dominic Groshong</p>
-</footer>
--- HW1/index.php
+++
@@ -1,7 +0,0 @@
-< ?php include 'header.php'; ?>
-
-<body>
-  <div><p>Hello World</p></div>
-</body>
-
-< ?php include 'footer.php'; ?>
