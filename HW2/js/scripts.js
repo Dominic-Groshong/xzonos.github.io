dcaaa5db128e3cd3a84287de14e93f61aaf5e1d7
Dominic Groshong <dominic.groshong@5ivemarketing.com>
2018-10-02 18:22:02 -0700

    Adding bootstrap classes; Wrote fuctions to calcualte values;

---
+++ HW2/js/scripts.js
@@ -0,0 +1,66 @@
+function findCash(){
+	var arr = document.getElementsByName('cash');
+	var total = 0;
+	for(var i=0; i<arr.length; i++){
+		if(parseInt(arr[i].value))
+			total += parseInt(arr[i].value);
+	}
+
+	return total;
+}
+
+function findInvested(){
+	var arr = document.getElementsByName('invested');
+	var total = 0;
+	for(var i=0; i<arr.length; i++){
+		if(parseInt(arr[i].value))
+			total += parseInt(arr[i].value);
+	}
+
+	return total;
+}
+
+function findRetirement(){
+	var arr = document.getElementsByName('retirement');
+	var total = 0;
+	for(var i=0; i<arr.length; i++){
+		if(parseInt(arr[i].value))
+			total += parseInt(arr[i].value);
+	}
+
+	return total;
+}
+
+function findBusiness(){
+	var arr = document.getElementsByName('business');
+	var total = 0;
+	for(var i=0; i<arr.length; i++){
+		if(parseInt(arr[i].value))
+			total += parseInt(arr[i].value);
+	}
+
+	return total;
+}
+
+function findUseAssets(){
+	var arr = document.getElementsByName('useAsset');
+	var total = 0;
+	for(var i=0; i<arr.length; i++){
+		if(parseInt(arr[i].value))
+			total += parseInt(arr[i].value);
+	}
+
+	return total;
+}
+
+function computeNetWorth(){
+	var cashTotal = findCash();
+	var investTotal = findInvested();
+	var retireTotal = findRetirement();
+	var busTotal = findBusiness();
+	var useTotal = findUseAssets();
+
+	var totalAssets = cashTotal + investTotal + retireTotal + busTotal + useTotal;
+
+	document.getElementById("netWorth").innerHTML = "<h3>Net Worth: " + totalAssets + "</h3>";
+}
