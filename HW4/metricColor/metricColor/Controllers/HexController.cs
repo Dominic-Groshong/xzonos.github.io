5d2fde5fd1e98947b9beb7c741c955c4d08e9a79
Dominic Groshong <dominic.groshong@5ivemarketing.com>
2018-10-18 19:31:25 -0700

    Added razor textboxes and form

---
+++ HW4/metricColor/metricColor/Controllers/HexController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace metricColor.Controllers
+{
+    public class HexController : Controller
+    {
+        // GET: Hex
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+  }
+}
