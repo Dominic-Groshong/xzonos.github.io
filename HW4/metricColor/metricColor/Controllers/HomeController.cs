6070b9bdce6d4988adf4923c19cb846151816273
Dominic Groshong <dominic.groshong@5ivemarketing.com>
2018-10-18 18:42:51 -0700

    Finished homepage and converter

--- HW4/metricColor/metricColor/Controllers/HomeController.cs
+++ HW4/metricColor/metricColor/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,25 +6,67 @@ using System.Web.Mvc;
 
 namespace metricColor.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+      return View();
     }
+
+    [HttpGet]
+    public ActionResult Converter()
+    {
+      // If there is no get info avalible yet, return the view.
+      if (Request.QueryString["miles"] == null || Request.QueryString["unit"] == null)
+      {
+        return View();
+      }
+
+      // Preform the conversion
+      else
+      {
+
+        int miles = int.Parse(Request.QueryString["miles"]);
+        string unit = Request.QueryString["unit"];
+        double result = 0;
+
+        if (miles < 0)
+        {
+          ViewBag.conversion = "Positive numbers only my dude.";
+        }
+
+        else if (unit == "millimeters")
+        {
+          result = miles * 1609344;
+          ViewBag.conversion = miles + " miles equals " + result + " millimeters.";
+        }
+
+        else if (unit == "centimeters")
+        {
+          result = miles * 160934;
+          ViewBag.conversion = miles + " miles equals " + result + " centimeters.";
+        }
+
+        else if (unit == "meters")
+        {
+          result = miles * 1609.344;
+          ViewBag.conversion = miles + " miles equals " + result + " meters.";
+        }
+
+        else if (unit == "kilometers")
+        {
+          result = miles * 1.60934;
+          ViewBag.conversion = miles + " miles equals " + result + " kilometers.";
+        }
+        else
+        {
+          ViewBag.conversion = "That way only leeds to madness.";
+        }
+
+      }
+
+      return View();
+    }
+
+  }
 }
