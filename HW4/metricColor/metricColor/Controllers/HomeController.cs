ea0f61543f9a548c73fe380b1833c2c282dd9057
Dominic Groshong <dominic.groshong@5ivemarketing.com>
2018-10-21 19:43:10 -0700

    Added comments and prevented a workaround on the converter

--- HW4/metricColor/metricColor/Controllers/HomeController.cs
+++ HW4/metricColor/metricColor/Controllers/HomeController.cs
@@ -25,42 +25,59 @@ namespace metricColor.Controllers
       // Preform the conversion
       else
       {
-
-        int miles = int.Parse(Request.QueryString["miles"]);
-        string unit = Request.QueryString["unit"];
-        double result = 0;
-
-        if (miles < 0)
+        try
         {
-          ViewBag.conversion = "Positive numbers only my dude.";
+          // Parse the string value into a double
+          double miles = double.Parse(Request.QueryString["miles"]);
+
+          // Get the unit type
+          string unit = Request.QueryString["unit"];
+          double result = 0;
+
+          // Test to check for negitive numbers
+          if (miles < 0)
+          {
+            ViewBag.conversion = "Positive numbers only my dude.";
+          }
+
+          // Preform this conversion if unit is millimeters
+          else if (unit == "millimeters")
+          {
+            result = miles * 1609344;
+            ViewBag.conversion = miles + " miles equals " + result + " millimeters.";
+          }
+
+          // Preform this conversion if unit is centimeters
+          else if (unit == "centimeters")
+          {
+            result = miles * 160934;
+            ViewBag.conversion = miles + " miles equals " + result + " centimeters.";
+          }
+
+          // Preform this conversion if unit is meters
+          else if (unit == "meters")
+          {
+            result = miles * 1609.344;
+            ViewBag.conversion = miles + " miles equals " + result + " meters.";
+          }
+
+          // Preform this conversion if unit is kilometers
+          else if (unit == "kilometers")
+          {
+            result = miles * 1.60934;
+            ViewBag.conversion = miles + " miles equals " + result + " kilometers.";
+          }
+
+          // If we made it here something went wrong, someone changed something...
+          else
+          {
+            ViewBag.conversion = "That way only leeds to madness.";
+          }
         }
-
-        else if (unit == "millimeters")
+        // Catch if someone trys to navigate around the input.
+        catch (FormatException e)
         {
-          result = miles * 1609344;
-          ViewBag.conversion = miles + " miles equals " + result + " millimeters.";
-        }
-
-        else if (unit == "centimeters")
-        {
-          result = miles * 160934;
-          ViewBag.conversion = miles + " miles equals " + result + " centimeters.";
-        }
-
-        else if (unit == "meters")
-        {
-          result = miles * 1609.344;
-          ViewBag.conversion = miles + " miles equals " + result + " meters.";
-        }
-
-        else if (unit == "kilometers")
-        {
-          result = miles * 1.60934;
-          ViewBag.conversion = miles + " miles equals " + result + " kilometers.";
-        }
-        else
-        {
-          ViewBag.conversion = "That way only leeds to madness.";
+          ViewBag.conversion = "Yo, not cool. Use the input.";
         }
 
       }
