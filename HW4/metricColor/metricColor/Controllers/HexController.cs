5431c82fa21aee13d5d4643d805fede3dca582b1
Dominic Groshong <dominic.groshong@5ivemarketing.com>
2018-10-21 17:28:05 -0700

    Additive color mixer working

--- HW4/metricColor/metricColor/Controllers/HexController.cs
+++ HW4/metricColor/metricColor/Controllers/HexController.cs
@@ -7,39 +7,64 @@ using System.Web.Mvc;
 
 namespace metricColor.Controllers
 {
-    public class HexController : Controller
+  public class HexController : Controller
+  {
+    // GET: Hex
+    public ActionResult Index()
     {
-        // GET: Hex
-        public ActionResult Index()
-        {
-            return View();
-        }
+      return View();
+    }
 
-        [HttpPost]
-        public ActionResult Index(String userinput, String userInput2)
-        {
-          try
-          {
-           var firstColor = Request["firstColor"];
-           var secondColor = Request["secondColor"];
+    [HttpPost]
+    public ActionResult Index(String userinput, String userInput2)
+    {
+      try
+      {
+        // Get the colors from the submited form.
+        Color firstColor = ColorTranslator.FromHtml(Request["firstColor"]);
+        Color secondColor = ColorTranslator.FromHtml(Request["secondColor"]);
 
-           renderMix(firstColor, secondColor, "black");
+        //Combine the two colors to create a third
+        Color mixedColor = mixColor(firstColor, secondColor);
 
-            // Return the view
-            return View();
-          }
+        // Run the colors into the viewbag.
+        renderMix(firstColor, secondColor, mixedColor);
 
-          catch (NullReferenceException e)
-          {
-            return View();
-          }
+        // Return the view
+        return View();
+      }
 
-        }
+      catch (NullReferenceException e)
+      {
+        return View();
+      }
+
+    }
 
 
-        private void renderMix(String colorOne, String colorTwo, String colorThree)
-        {
-          ViewBag.RenderMix = "<div style='background: colorOne'> <p>" + colorOne + "</p></div>";
-        }
+    private Color mixColor(Color colorOne, Color colorTwo)
+    {
+      // Establish new color variable.
+      Color mixedColor;
+
+      // Get the combined values of the Red, Blue, Green fields devide by 2 to get average color.
+      int red = (colorOne.R + colorTwo.R) / 2;
+      int blue = (colorOne.B + colorTwo.B) / 2;
+      int green = (colorOne.G + colorTwo.G) / 2;
+
+      // Create the new color from the rgb int numbers.
+      mixedColor = Color.FromArgb(red, green, blue);
+
+      // return the combined color.
+      return mixedColor;
+    }
+
+    private void renderMix(Color colorOne, Color colorTwo, Color colorThree)
+    {
+      ViewBag.RenderMix =
+              "<div style='background: rgb(" + colorOne.R + "," + colorOne.G + "," + colorOne.B + "); height: 80px; width: 80px; '></div>" + " + "
+            + "<div style='background: rgb(" + colorTwo.R + "," + colorTwo.G + "," + colorTwo.B + "); height: 80px; width: 80px; '></div>" + " = "
+            + "<div style='background: rgb(" + colorThree.R + "," + colorThree.G + "," + colorThree.B + "); height: 80px; width: 80px; '></div>";
+    }
   }
 }
