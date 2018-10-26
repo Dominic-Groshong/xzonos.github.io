8c31c29d3918881fe258ac289e14ec95e623af1a
Dominic Groshong <dominic.groshong@5ivemarketing.com>
2018-10-26 10:57:03 -0700

    Fixed model to allow controller creation

--- HW5/database/database/Controllers/HomeController.cs
+++ HW5/database/database/Controllers/HomeController.cs
@@ -10,27 +10,10 @@ namespace database.Controllers
   public class HomeController : Controller
   {
 
-    public AssistanceRequest Model = new AssistanceRequest();
-
     public ActionResult Index()
     {
       return View();
     }
 
-    public ActionResult FormRequest()
-    {
-      return View(Model);
-    }
-
-    [HttpPost]
-    public ActionResult FormRequest(AssistanceRequest request)
-    {
-      if (ModelState.IsValid)
-      {
-        // TODO: Add request to requst collection
-        return Redirect("~/Index");
-      }
-      return View();
-    }
   }
 }
--- HW5/database/database/Models/AssistanceRequest.cs
+++ HW5/database/database/Models/AssistanceRequest.cs
@@ -9,22 +9,39 @@ namespace database.Models
 
   public class AssistanceRequest
   {
+    [Key]
+    public int ID { get; set; }
+
     [Display(Name = "First Name")]
+    [Required]
     public string FirstName { get; set; }
 
     [Display(Name = "Last Name")]
+    [Required]
     public string LastName { get; set; }
 
     [Phone]
+    [Required]
     public string Phone { get; set; }
 
+    [Required]
     public string Building { get; set; }
 
+    [Required]
     public int Suite { get; set; }
 
-    public string Message { get; set; }
+    [Required]
+    public string Comments { get; set; }
 
     [Display(Name = "Select here to give permission for the landlord or representitive to enter your suite to preform the requested maintenance. We wil call first.")]
     public bool Access { get; set; }
+
+    private DateTime date = DateTime.Now;
+    [Display(Name = "Time Requested")]
+    public DateTime RequestAt
+    {
+      get { return date; }
+      set { date = value; }
+    }
   }
 }
